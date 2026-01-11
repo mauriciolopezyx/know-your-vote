bafac4fb3afa43ff7900973f3304533788458b14
orangetbh <mauriciolopezyx@gmail.com>
2026-01-11 00:18:23 -0800

    producting deploying again......(2)

--- frontend/components/theme-dropdown.tsx
+++ frontend/components/theme-dropdown.tsx
@@ -24,7 +24,7 @@ export default function ThemeDropdown() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="secondary" className="rounded-full" size="icon" onClick={changeTheme}>
+                <Button variant="secondary" className="cursor-pointer rounded-full" size="icon" onClick={changeTheme}>
                     <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
                     <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
                     <span className="sr-only">Toggle Theme</span>
